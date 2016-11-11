715f31bf7a0cf4742346c5d8964e2aec53461a3e
Petter Arvidsson <petter.arvidsson@gmail.com>
2016-11-11 16:29:42 +0100

    Fix glad build, sort of

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,3 @@
-#if defined(NANOGUI_GLAD)
-    #if defined(NANOGUI_SHARED) && !defined(GLAD_GLAPI_EXPORT)
-        #define GLAD_GLAPI_EXPORT
-    #endif
-
-    #include <glad/glad.h>
-#else
-    #if defined(__APPLE__)
-        #define GLFW_INCLUDE_GLCOREARB
-    #else
-        #define GL_GLEXT_PROTOTYPES
-    #endif
-#endif
-
-#include <GLFW/glfw3.h>
-
 #include <nanogui/nanogui.h>
 
 #if defined(WIN32)
